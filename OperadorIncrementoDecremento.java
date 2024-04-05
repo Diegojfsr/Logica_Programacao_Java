d30f5c38374e45fd7ee5587584bf4e47ca93780f
DiegoJfsr <diegojfsr@gmail.com>
2024-04-05 12:13:17 -0300

    OperadorIncrementoDecremento

---
+++ OperadorIncrementoDecremento.java
@@ -0,0 +1,30 @@
+public class OperadorIncrementoDecremento {
+    public static void main(String[] args) {
+
+        int meuIncremento = 1;
+        System.out.println("Valor da Variavel ANTES do Incremento: " + meuIncremento);
+            meuIncremento++;
+            meuIncremento++;
+            meuIncremento++;
+            meuIncremento++;
+            meuIncremento++;
+            meuIncremento++;
+        System.out.println("Valor da Variavel DEPOIS do Incremento: " + meuIncremento);
+
+        int meuDecremento = 10;
+        System.out.println("Valor da Variavel ANTES do Decremento: " + meuDecremento);
+            meuDecremento--;
+            meuDecremento--;
+            meuDecremento--;
+            meuDecremento--;
+            meuDecremento--;
+            meuDecremento--;
+        System.out.println("Valor da Variavel DEPOIS do Decremento: " + meuDecremento);
+
+
+
+
+
+
+    }    
+}
