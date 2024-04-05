b3ceaeae8488e3a9ed68a5fb34c7cccee2d38d4d
DiegoJfsr <diegojfsr@gmail.com>
2024-04-05 12:04:11 -0300

    Operadores Matematicos

---
+++ OperadoresMatematicos.java
@@ -0,0 +1,32 @@
+public class OperadoresMatematicos {
+    public static void main(String[] args) {
+        //Operador Atribuicao
+        double salario = 100000.0;
+
+        //Operador Matematico
+        // Soma
+        int soma = 26 + 25;
+        System.out.println(soma);
+
+        // Soma
+        int subtracao = 26 - 25;
+        System.out.println(subtracao);
+
+        //Multiplicacao
+        int multiplicacao = 26 * 25;
+        System.out.println(multiplicacao);
+
+        //Divisao
+        int divisao = 26 * 25;
+        System.out.println(divisao);
+
+        //Modulo / Resto
+        int modulo = 26 % 4;
+        System.out.println(modulo);
+
+        //Divisao exata
+        double idade = (double)26 / 4;
+        System.out.println(idade);
+
+    }    
+}
