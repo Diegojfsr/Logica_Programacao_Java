dbbea4ed329a677de2129b0bde1be902c079b5e2
DiegoJfsr <diegojfsr@gmail.com>
2024-04-05 11:39:26 -0300

    Variaveis

---
+++ Variaveis.java
@@ -0,0 +1,29 @@
+public class Variaveis {
+    public static void main(String[] args) {
+
+        byte variavelByte = 127;
+        System.out.println(variavelByte);
+
+        short variavelShort = 32767;
+        System.out.println(variavelShort);
+
+        int variavelInt = 214783647;
+        System.out.println(variavelInt);
+
+        long variavelLong = 10000000L;
+        System.out.println(variavelLong);
+
+        float variavelFloat = 132.4F;
+        System.out.println(variavelFloat);
+
+        double variavelDouble = 123456789.1;
+        System.out.println(variavelDouble);
+
+        char variavelChar = 'a';
+        System.out.println(variavelChar);
+
+        boolean variavelBoolean = true;
+        System.out.println(variavelBoolean);
+    }
+
+}
