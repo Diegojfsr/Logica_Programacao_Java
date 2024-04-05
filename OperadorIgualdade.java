792d06291720c3cdbe5d1272b86b8295cfcd6ebf
DiegoJfsr <diegojfsr@gmail.com>
2024-04-05 12:21:36 -0300

    OperadorIgualdade

---
+++ OperadorIgualdade.java
@@ -0,0 +1,23 @@
+public class OperadorIgualdade {
+    public static void main(String[] args) {
+        
+        char letraA = 'a';
+        char letraB = 'b';
+
+        if(letraA == letraB){
+            System.out.println("São iguais!");
+        }else{
+            System.out.println("São Diferentes!");
+        };
+
+
+        long valor1 = 2L;
+        long valor2 = 1L;
+
+        if(valor1!=valor2){
+            System.out.println("São Diferentes!");
+        }else{
+            System.out.println("São iguais!");
+        }
+    }
+}
