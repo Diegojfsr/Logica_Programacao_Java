80d285998433a3ee8e24183fb56dc1b015f7ba2e
DiegoJfsr <diegojfsr@gmail.com>
2024-04-07 19:53:19 -0300

    OperadorAtribuicao

---
+++ OperadorAtribuicao.java
@@ -0,0 +1,10 @@
+public class OperadorAtribuicao {
+    public static void main(String[] args) {
+        double salario = 1000000.0;
+        int salario2 = 4000;
+
+        System.out.println(salario);
+        System.out.println(salario2);
+    }
+    
+}
