61ef23cf7707f09dbcf5f8f9c8e7e98c8a91ff43
DiegoJfsr <diegojfsr@gmail.com>
2024-04-06 21:14:25 -0300

    Operadores Logicos

---
+++ OperadoresLogicos.java
@@ -0,0 +1,11 @@
+public class OperadoresLogicos {
+    public static void main(String[] args) {
+
+        boolean verdadeiro = true;
+        boolean  falso = false;
+
+        System.out.println(verdadeiro || falso);
+        System.out.println(verdadeiro && falso);
+    }
+    
+}
